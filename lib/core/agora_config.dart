922b8225c963398d6a19349fff738c1bf86b7c69
Addan206 <105232087+Addan206@users.noreply.github.com>
2026-02-04 15:39:39 +0500

    initial commit

---
+++ lib/core/agora_config.dart
@@ -0,0 +1,10 @@
+class AgoraConfig {
+  static const String appId =
+      "aff156bcae384b2098c12a6c7739a988";
+
+  static const String token =
+      "007eJxTYLDmXvRjy/zEkq+vGPsk3iWJ8S1ut355486eqw0Z30TqmM0VGBLT0gxNzZKSE1ONLUySjAwsLZINjRLNks3NjS0TLS0sbsk2ZzYEMjJ4F39hYIRCEJ+doSS1uMTQyJiBAQD1kyDm";
+
+  static const String channelName = "test123";
+  static const int localUid = 1;
+}
